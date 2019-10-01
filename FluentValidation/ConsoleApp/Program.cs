4193452bc120dcba4aa53967c4cc6c5211ec3a33
Eric Cleveland <eric@clevelandcloud.com>
2019-10-01 17:18:31 -0600

    Trying this stuff out.

--- FluentValidation/ConsoleApp/Program.cs
+++ FluentValidation/ConsoleApp/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Text.Json;
+
+using FluentValidation;
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Beginning Fluent Validation proof of concept.");
+
+            User user = new User();
+            user.Id = 1;
+            UserValidator validator = new UserValidator();
+
+            var output = JsonSerializer.Serialize(user, options: new JsonSerializerOptions{
+                WriteIndented = true
+            });
+            Console.WriteLine(output);
+
+            var result = validator.Validate(user);
+
+            foreach (var error in result.Errors)
+            {
+                Console.WriteLine($"Property {error.PropertyName} failed validation. Error was: {error.ErrorMessage}");
+            }
+        }
+    }
+
+    public class User 
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class UserValidator : AbstractValidator<User> 
+    {
+        public UserValidator() {
+            RuleFor(user => user.FirstName).NotNull();
+            RuleFor(user => user.LastName).NotNull();
+            RuleFor(user => user.Id).GreaterThanOrEqualTo(0);
         }
     }
 }
