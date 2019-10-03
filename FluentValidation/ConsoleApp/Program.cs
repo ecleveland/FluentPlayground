23ea70fdcd31f06e482da571aafb2018030e904a
Eric Cleveland <eric@clevelandcloud.com>
2019-10-03 15:34:13 -0600

    Basic implementation added. New Blazor app added

--- FluentValidation/ConsoleApp/Program.cs
+++ FluentValidation/ConsoleApp/Program.cs
@@ -12,20 +12,36 @@ namespace ConsoleApp
             Console.WriteLine("Beginning Fluent Validation proof of concept.");
 
             User user = new User();
+            user.FirstName = "Fool";
             user.Id = 1;
-            UserValidator validator = new UserValidator();
+            UserValidator simpleValidator = new UserValidator();
 
+            // Could talk about the JsonSearializer in the new System.Text.Json
             var output = JsonSerializer.Serialize(user, options: new JsonSerializerOptions{
                 WriteIndented = true
             });
+
             Console.WriteLine(output);
 
-            var result = validator.Validate(user);
+            var simpleValidation = simpleValidator.Validate(user);
 
-            foreach (var error in result.Errors)
+            foreach (var error in simpleValidation.Errors)
             {
                 Console.WriteLine($"Property {error.PropertyName} failed validation. Error was: {error.ErrorMessage}");
             }
+            
+
+            // try
+            // {
+            //     sValidate.ValidateAndThrow(user);
+            // }
+            // catch (ValidationException e)
+            // {
+            //     foreach (var error in e.Errors)
+            //     {
+            //         Console.WriteLine($"{error.ErrorCode}");
+            //     }
+            // }
         }
     }
 
@@ -34,6 +50,7 @@ namespace ConsoleApp
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public Address Address { get; set; }
     }
 
     public class UserValidator : AbstractValidator<User> 
@@ -42,6 +59,23 @@ namespace ConsoleApp
             RuleFor(user => user.FirstName).NotNull();
             RuleFor(user => user.LastName).NotNull();
             RuleFor(user => user.Id).GreaterThanOrEqualTo(0);
+            //RuleFor(customer => customer.Address).SetValidator(new AddressValidator());
+            RuleFor(customer => customer.Address.ZipCode).NotNull().When(customer => customer.Address != null);
         }
     }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+    }
+
+    // class AddressValidator : AbstractValidator<Address>
+    // {
+    //     public AddressValidator()
+    //     {
+    //         RuleFor(address => address.ZipCode).NotNull().NotEmpty();
+    //     }
+    // }
 }
